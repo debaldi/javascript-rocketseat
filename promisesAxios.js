35835e968aa82f215c6ecc59f226c074bfc96904
Emerson de Baldi <edbemerson@gmail.com>
2022-03-23 21:13:33 -0400

    update

---
+++ promisesAxios.js
@@ -0,0 +1,8 @@
+// Promises com Axios - Pomessas com Axios e encadeamento
+import axios from "axios";
+
+axios
+.get('https://api.github.com/users/debaldi')
+.then( response => axios.get(response.data.repos_url) )
+.then( repos => console.log(repos.data) )
+.catch( error => console.log(error))
