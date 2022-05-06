42f338e2122e025f5ee0106b08e7564fd0c22377
Emerson de Baldi <edbemerson@gmail.com>
2022-05-05 20:17:15 -0400

    Desafio Modal

--- DOM/Desafio-modal/script.js
+++ DOM/Desafio-modal/script.js
@@ -2,9 +2,24 @@ const buttonOpenModal = document.getElementById('openModal')
 
 const modalWrapper = document.querySelector('.modal-wrapper')
 
+const modalWrapperId = document.getElementById('modal-wrapper-id')
+
 buttonOpenModal.onclick = function() {
     modalWrapper
         .classList
         .remove('invisible')
 }
 
+document.addEventListener('keydown', function(event){
+    
+    const isEscKey = event.key === "Escape"
+    
+    const isInvisibleClass = modalWrapperId.classList.contains('invisible')
+
+    if(isInvisibleClass == false){        
+        if(isEscKey){
+            modalWrapper.classList.add('invisible')
+            console.log(event.key)
+        }
+    }
+})
